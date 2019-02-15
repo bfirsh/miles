9322cc3cff90bb6e28d04a5b98e3bf3cacf901db
Ben Firshman <ben@firshman.co.uk>
2019-02-15 11:34:22 -0800

    Flatten App into function

--- example/client/App.js
+++ example/client/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import TodoListController from "./controllers/todoList";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/" component={TodoListController} />
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Route exact path="/" component={TodoListController} />
+  </Router>
+);
 
 export default App;
--- template/client/App.js
+++ template/client/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import HomeController from "./controllers/home";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/" component={HomeController} />
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Route exact path="/" component={HomeController} />
+  </Router>
+);
 
 export default App;
